5359b12674db825627a72c1e59d19a3e47cf24a3
Kim SeungHyun <korafa@naver.com>
2024-03-18 00:06:02 +0900

    3DcourseDefence

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private List <Waypoint> path = new List <Waypoint> ();
+    [SerializeField] [Range(0f, 5f)] private float speed = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(FollowPath ());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }       
+    }
+}
+
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+
+    private void OnMouseDown()
+    {
+        if (isPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
+        
+    }
+}
